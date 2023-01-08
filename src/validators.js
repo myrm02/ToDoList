d239b4f80f76b7f6eb76b19abf813cc5c5ceca10
Myriam BENABDESSADOK <myriamb1512@gmail.com>
2023-01-08 23:52:54 +0100

    Modification validator requirement

--- src/validators.js
+++ src/validators.js
@@ -1,14 +1,3 @@
 import * as yup from "yup"
 
-export const descriptionValidator = yup
-  .string()
-  .matches(
-    /^[a-z][a-z0-9_]{1,}$/,
-    "Your username must start with a lower case letter and contain only digits or letters or `_`"
-  )
-  .label("Description")
-
-// export const favoriteColorValidator = yup
-//   .string()
-//   .matches(/^#[0-9a-f]{6}$/, "Color must be in #RRGGBB format")
-//   .label("Favorite color")
+export const descriptionValidator = yup.string().label("Description")
